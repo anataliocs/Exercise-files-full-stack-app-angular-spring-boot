ffc1001c4568249fd915c9d9b523a1acd7dfb4fa
chris.anatalio <anataliocs@gmail.com>
2017-03-26 12:06:58 -0700

    01_07_End_Implement-a-GET-Endpoint-using-Spring-MVC

---
+++ src/main/java/com/linkedin/learning/model/Self.java
@@ -0,0 +1,14 @@
+package com.linkedin.learning.model;
+
+public class Self {
+
+    private String ref;
+
+    public String getRef() {
+        return ref;
+    }
+
+    public void setRef(String ref) {
+        this.ref = ref;
+    }
+}
---
+++ src/main/java/com/linkedin/learning/rest/ResourceConstants.java
@@ -0,0 +1,5 @@
+package com.linkedin.learning.rest;
+
+public class ResourceConstants {
+    public static final String ROOM_RESERVATION_V1 = "/room/reservation/v1";
+}
