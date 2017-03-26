3e35e9524fbce80a0b44ed39ab2fb009497a981d
chris.anatalio <anataliocs@gmail.com>
2017-03-26 10:16:04 -0700

    01_03: Initial commit

---
+++ src/main/java/com/linkedin/learning/LinkedInLearningFullStackAppAngularSpringBootApplication.java
@@ -0,0 +1,12 @@
+package com.linkedin.learning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinkedInLearningFullStackAppAngularSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LinkedInLearningFullStackAppAngularSpringBootApplication.class, args);
+	}
+}
