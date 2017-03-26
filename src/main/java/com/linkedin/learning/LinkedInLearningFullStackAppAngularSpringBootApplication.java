127794acbae9ff8812318ca772be229633b136ae
chris.anatalio <anataliocs@gmail.com>
2017-03-26 10:38:31 -0700

    01_05_End_Configure-your-API-using-Spring-Java-Config

--- src/main/java/com/linkedin/learning/LinkedInLearningFullStackAppAngularSpringBootApplication.java
+++ src/main/java/com/linkedin/learning/LinkedInLearningFullStackAppAngularSpringBootApplication.java
@@ -1,12 +1,16 @@
 package com.linkedin.learning;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@EnableAutoConfiguration
+@ComponentScan
 public class LinkedInLearningFullStackAppAngularSpringBootApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(LinkedInLearningFullStackAppAngularSpringBootApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(LinkedInLearningFullStackAppAngularSpringBootApplication.class, args);
+    }
 }
