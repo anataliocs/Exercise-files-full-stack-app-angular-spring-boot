d611979a0e7fd446dd6d3fe2b54da2bdc00c1a20
chris.anatalio <anataliocs@gmail.com>
2017-03-26 20:33:43 -0700

    01_09_Implement-a-POST-PUT-and-DELETE-Endpoint-using-Spring-MVC

--- src/main/java/com/linkedin/learning/config/ApiConfig.java
+++ src/main/java/com/linkedin/learning/config/ApiConfig.java
@@ -1,15 +1,20 @@
 package com.linkedin.learning.config;
 
+
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.ObjectWriter;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
 
 @Configuration
 public class ApiConfig {
 
-    @Bean
     public ObjectMapper objectMapper() {
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.registerModule(new JavaTimeModule());
+
         return new ObjectMapper();
     }
 
