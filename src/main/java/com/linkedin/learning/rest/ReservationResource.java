d5919ef706df43a6042577b2558126dbcd5a46ef
chris.anatalio <anataliocs@gmail.com>
2017-03-26 20:33:43 -0700

    01_09_Implement-a-POST-PUT-and-DELETE-Endpoint-using-Spring-MVC

--- src/main/java/com/linkedin/learning/rest/ReservationResource.java
+++ src/main/java/com/linkedin/learning/rest/ReservationResource.java
@@ -1,10 +1,14 @@
 package com.linkedin.learning.rest;
 
+import com.linkedin.learning.model.request.ReservationRequest;
 import com.linkedin.learning.model.response.ReservationResponse;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -26,5 +30,31 @@ public class ReservationResource {
             LocalDate checkout) {
 
         return new ResponseEntity<>(new ReservationResponse(), HttpStatus.OK);
+    }
+
+    @RequestMapping(path = "", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_UTF8_VALUE,
+            consumes = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public ResponseEntity<ReservationResponse> createReservation(
+            @RequestBody
+            ReservationRequest reservationRequest) {
+
+        return new ResponseEntity<>(new ReservationResponse(), HttpStatus.CREATED);
+    }
+
+    @RequestMapping(path = "", method = RequestMethod.PUT, produces = MediaType.APPLICATION_JSON_UTF8_VALUE,
+            consumes = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public ResponseEntity<ReservationResponse> updateReservation(
+            @RequestBody
+            ReservationRequest reservationRequest) {
+
+        return new ResponseEntity<>(new ReservationResponse(), HttpStatus.OK);
+    }
+
+    @RequestMapping(path = "/{reservationId}", method = RequestMethod.DELETE)
+    public ResponseEntity<Void> deleteReservation(
+            @PathVariable
+            long reservationId) {
+
+        return new ResponseEntity<Void>(HttpStatus.NO_CONTENT);
     }
 }
