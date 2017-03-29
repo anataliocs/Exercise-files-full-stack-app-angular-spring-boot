52dd50a8382ed74eb4e113c3546c3f036a27ef54
chris.anatalio <anataliocs@gmail.com>
2017-03-29 14:45:32 -0700

    03_02_Return-Pageable-List-of-Rooms

--- src/main/java/com/linkedin/learning/config/ConversionConfig.java
+++ src/main/java/com/linkedin/learning/config/ConversionConfig.java
@@ -1,5 +1,6 @@
 package com.linkedin.learning.config;
 
+import com.linkedin.learning.convertor.RoomEntityToReservableRoomResponseConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ConversionServiceFactoryBean;
@@ -13,6 +14,7 @@ import java.util.Set;
 
         private Set<Converter> getConverters() {
                 Set<Converter> converters = new HashSet<Converter>();
+                converters.add(new RoomEntityToReservableRoomResponseConverter());
 
                 return converters;
         }
---
+++ src/main/java/com/linkedin/learning/model/response/ReservableRoomResponse.java
@@ -0,0 +1,48 @@
+package com.linkedin.learning.model.response;
+
+import com.linkedin.learning.model.Links;
+
+public class ReservableRoomResponse {
+
+	private Long id;
+	private Integer roomNumber;
+	private Integer price;
+	private Links links;
+	
+	public ReservableRoomResponse() {
+		super();
+	}
+	
+	public ReservableRoomResponse(Integer roomNumber, Integer price) {
+		super();
+		this.roomNumber = roomNumber;
+		this.price = price;
+	}
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public Integer getRoomNumber() {
+		return roomNumber;
+	}
+	public void setRoomNumber(Integer roomNumber) {
+		this.roomNumber = roomNumber;
+	}
+	public Integer getPrice() {
+		return price;
+	}
+	public void setPrice(Integer price) {
+		this.price = price;
+	}
+	public Links getLinks() {
+		return links;
+	}
+	public void setLinks(Links links) {
+		this.links = links;
+	}
+	
+	
+}
