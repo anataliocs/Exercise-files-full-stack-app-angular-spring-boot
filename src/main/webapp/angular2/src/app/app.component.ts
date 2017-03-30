7d34f32581025bea98fd11873606b47993fc70a1
chris.anatalio <anataliocs@gmail.com>
2017-03-29 19:17:13 -0700

    04_01_Implement-List-Rooms-REST-API-Request

--- src/main/webapp/angular2/src/app/app.component.ts
+++ src/main/webapp/angular2/src/app/app.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
+import {Http, Response} from "@angular/http";
+import {Observable} from "rxjs/Rx";
+import "rxjs/add/operator/map";
+import "rxjs/add/operator/catch";
 
 @Component({
   selector: 'app-root',
@@ -8,6 +12,10 @@ import { FormControl, FormGroup } from '@angular/forms';
 })
 export class AppComponent implements OnInit {
 
+  constructor(private http:Http) {
+  }
+
+  private baseUrl:string = 'http://localhost:8080';
   public submitted:boolean;
   roomsearch : FormGroup;
   rooms: Room[];
@@ -18,16 +26,34 @@ export class AppComponent implements OnInit {
             checkout: new FormControl('')
         });
 
-        this.rooms = ROOMS;
     }
 
-    onSubmit({value,valid}: {value:Roomsearch, valid:boolean}) {
-      console.log(value);
-    }
+  onSubmit({value, valid}: { value:Roomsearch, valid:boolean }) {
+
+    this.getAll()
+      .subscribe(
+        rooms => this.rooms = rooms,
+        err => {
+          // Log errors if any
+          console.log(err);
+        });
+  }
 
     reserveRoom(value:string) {
       console.log("Room id for reservation:" + value);
     }
+
+  getAll():Observable<Room[]> {
+
+    //noinspection TypeScriptValidateTypes
+    return this.http
+      .get(`${this.baseUrl}/room/reservation/v1/?checkin=2017-03-18&checkout=2016-03-18`)
+      .map(this.mapRoom);
+  }
+
+  mapRoom(response:Response):Room[] {
+    return response.json().content;
+  }
 
 }
 
@@ -43,23 +69,3 @@ export interface Room {
   links: string;
 }
 
-var ROOMS:Room[] = [
-  {
-      "id": "37489234327",
-      "roomNumber": "406",
-      "price": "25",
-      "links": ""
-  },
-  {
-    "id": "84329874798",
-      "roomNumber": "407",
-      "price": "20",
-      "links": ""
-  },
-  {
-    "id": "17238423787",
-      "roomNumber": "408",
-      "price": "22",
-      "links": ""
-  }
-];
