07c41c5a8f5e137b3cdbb6c407e4e4c30f76bb6f
chris.anatalio <anataliocs@gmail.com>
2017-03-29 19:17:13 -0700

    04_01_Implement-List-Rooms-REST-API-Request

--- src/main/java/com/linkedin/learning/rest/ReservationResource.java
+++ src/main/java/com/linkedin/learning/rest/ReservationResource.java
@@ -18,6 +18,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -29,6 +30,7 @@ import java.time.LocalDate;
 
 @RestController
 @RequestMapping(ResourceConstants.ROOM_RESERVATION_V1)
+@CrossOrigin
 public class ReservationResource {
 
         @Autowired
